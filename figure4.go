dc0b23f712fbd24961103678623b4d70a49c2ecc
Gary Yu <gairy.yu@gmail.com>
2018-03-29 15:39:42 +0800

    Algorithm 1: Selection of a blue set. The example on page 16 Fig.4.

--- figure4.go
+++ figure4.go
@@ -1,35 +1,30 @@
+
+// Copyright 2018 The go-phantom Authors
+// This file is part of the go-phantom library.
+//
+// The go-phantom library is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Lesser General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// The go-phantom library is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU Lesser General Public License for more details.
+//
+// You should have received a copy of the GNU Lesser General Public License
+// along with the go-phantom library. If not, see <http://www.gnu.org/licenses/>.
+
 package main
 
 import (
-	"os"
 	"fmt"
-	"sort"
-	."phantom/phantom"
+	."github.com/garyyu/go-phantom/phantom"
+	."github.com/garyyu/go-phantom/utils"
 )
 
 var chain map[string]*Block
 
-func chainAddBlock(Name string, References []string, chain map[string]*Block) *Block{
-
-	//create this block
-	thisBlock := Block{Name, -1, make(map[string]*Block), make(map[string]*Block),make(map[string]bool)}
-
-	//add references
-	for _, Reference := range References {
-		prev, ok := chain[Reference]
-		if ok {
-			thisBlock.Prev[Reference] = prev
-			prev.Next[Name] = &thisBlock
-		}else{
-			fmt.Println("chainAddBlock(): error! block reference invalid. block name =", Name, " references=", Reference)
-			os.Exit(-1)
-		}
-	}
-
-	//add this block to the chain
-	chain[Name] = &thisBlock
-	return &thisBlock
-}
 
 func chainInitialize() map[string]*Block{
 
@@ -38,34 +33,34 @@ func chainInitialize() map[string]*Block{
 
 	//add blocks
 
-	chainAddBlock("Genesis", []string{}, chain)
+	ChainAddBlock("Genesis", []string{}, chain)
 
-	chainAddBlock("B", []string{"Genesis"}, chain)
-	chainAddBlock("C", []string{"Genesis"}, chain)
-	chainAddBlock("D", []string{"Genesis"}, chain)
-	chainAddBlock("E", []string{"Genesis"}, chain)
+	ChainAddBlock("B", []string{"Genesis"}, chain)
+	ChainAddBlock("C", []string{"Genesis"}, chain)
+	ChainAddBlock("D", []string{"Genesis"}, chain)
+	ChainAddBlock("E", []string{"Genesis"}, chain)
 
-	chainAddBlock("F", []string{"B","C"}, chain)
-	chainAddBlock("I", []string{"C","D"}, chain)
-	chainAddBlock("H", []string{"E"}, chain)
+	ChainAddBlock("F", []string{"B","C"}, chain)
+	ChainAddBlock("I", []string{"C","D"}, chain)
+	ChainAddBlock("H", []string{"E"}, chain)
 
-	chainAddBlock("J", []string{"F","D"}, chain)
-	chainAddBlock("L", []string{"F"}, chain)
-	chainAddBlock("K", []string{"J","I","E"}, chain)
-	chainAddBlock("N", []string{"D","H"}, chain)
+	ChainAddBlock("J", []string{"F","D"}, chain)
+	ChainAddBlock("L", []string{"F"}, chain)
+	ChainAddBlock("K", []string{"J","I","E"}, chain)
+	ChainAddBlock("N", []string{"D","H"}, chain)
 
-	chainAddBlock("M", []string{"L","K"}, chain)
-	chainAddBlock("O", []string{"K"}, chain)
-	chainAddBlock("P", []string{"K"}, chain)
-	chainAddBlock("Q", []string{"N"}, chain)
+	ChainAddBlock("M", []string{"L","K"}, chain)
+	ChainAddBlock("O", []string{"K"}, chain)
+	ChainAddBlock("P", []string{"K"}, chain)
+	ChainAddBlock("Q", []string{"N"}, chain)
 
-	chainAddBlock("R", []string{"O","P","N"}, chain)
+	ChainAddBlock("R", []string{"O","P","N"}, chain)
 
-	chainAddBlock("S", []string{"Q"}, chain)
-	chainAddBlock("T", []string{"S"}, chain)
-	chainAddBlock("U", []string{"T"}, chain)
+	ChainAddBlock("S", []string{"Q"}, chain)
+	ChainAddBlock("T", []string{"S"}, chain)
+	ChainAddBlock("U", []string{"T"}, chain)
 
-	chainAddBlock("Virtual", []string{"M","R", "U"}, chain)
+	ChainAddBlock("Virtual", []string{"M","R", "U"}, chain)
 
 	fmt.Println("chainInitialize(): done. blocks=", len(chain)-1)
 
@@ -82,26 +77,24 @@ func main() {
 
 	// print the result of blue sets
 
-	names := make([]string, len(chain))
-	i := 0
-	for name := range chain {
-		names[i] = name
-		i++
-	}
-	sort.Strings(names)
+	ltpq := LTPQ(chain, true)
 
-	fmt.Print("CalcBlue(): done completely. blue blocks = (G).")
-	for _, name := range names {
-		if name=="Genesis" || name=="Virtual" {
-			continue
-		}
+	fmt.Println("\n- Phantom Paper Simulation - Algorithm 1: Selection of a blue set. -")
+	fmt.Println("-                    The example on page 16 Fig.4.                 -\n")
 
+	fmt.Print("blue set selection done. blue blocks = ")
+	nBlueBlocks := 0
+	for _, name := range ltpq {
 		block := chain[name]
-		for _,blue := range block.Blue {
-			if blue==true {
+		if IsBlueBlock(block)==true {
+			if name=="Genesis" || name=="Virtual" {
+				fmt.Print("(",name[:1],").")
+			}else {
 				fmt.Print(block.Name, ".")
 			}
+
+			nBlueBlocks++
 		}
 	}
-	fmt.Println("(V)")
+	fmt.Println("	total blue:", nBlueBlocks)
 }
